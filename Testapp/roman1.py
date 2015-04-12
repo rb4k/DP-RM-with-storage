15175a6b15fc99462577f478a5e6cc9253a6db92
rob4ik@yahoo.de <rob4ik@yahoo.de>
2015-04-12 13:46:35 +0200

    Tests bei Roman1

--- Testapp/roman1.py
+++ Testapp/roman1.py
@@ -14,10 +14,49 @@ roman_numeral_map = (('M',  1000),
 
 def to_roman(n):
     '''convert integer to Roman numeral'''
+    if not (0 < n < 4000):
+        raise OutOfRangeError('number out of range (must be less than 4000)')
+    if not isinstance(n, int):
+        raise NonIntegerError('non-integers can not be converted')
+
     result = ''
     for numeral, integer in roman_numeral_map:
         while n >= integer:
             result += numeral
             n -= integer
             print('subtracting {0} from input, adding {1} to output'.format(integer, numeral))
+    return result
+
+class OutOfRangeError(ValueError):
+    pass
+
+class NonIntegerError(ValueError):
+    pass
+
+class InvalidRomanNumeralError(ValueError):
+    pass
+
+import re
+
+roman_numeral_pattern = re.compile('''
+    ^                   # beginning of string
+    M{0,3}              # thousands - 0 to 3 Ms
+    (CM|CD|D?C{0,3})    # .. S. 161
+    (XC|XL|L?X{0,3})    # ..
+    (IX|IV|V?I{0,3})    # ..
+    $                   # ..
+    ''', re.VERBOSE)
+
+def from_roman(s):
+    '''convert Roman numeral to integer'''
+    if not roman_numeral_pattern.search(s):
+        raise InvalidRomanNumeralError('Invalid Roman numeral: {0}'.format(s))
+
+    result = 0
+    index = 0
+    for numeral, integer in roman_numeral_map:
+        while s[index:index+len(numeral)] == numeral:
+            result += integer
+            index += len(numeral)
+            print('found', numeral, 'of length', len(numeral), ', adding', integer)
     return result
